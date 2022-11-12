2d6368041f5335ba81b5ce8d2beee18bb266a838
Miguel Hilser <mygue0908@gmail.com>
2022-11-12 17:16:53 -0300

    modificacion js

--- index.js
+++ index.js
@@ -11,49 +11,52 @@ function Bienvenido(){
     }while(valor);
     alert('Bienvenido '+nombre);
 }
+//clase producto
+class Producto{
+    constructor(id, nombreP, precio){
+        this.id=id,
+        this.nombreP=nombreP,
+        this.precio=precio;
+    }
+}
+const cuadro=new Producto(1, 'Cuadro', 1200);
+const mate=new Producto(2, 'Mate', 1000);
+const promocion=new Producto(3, 'Promoción', 800);
+
 function Compra(produc){
     let precio=0;
     let i=0;
     let articulo;
-    let articulos;
-    if(produc>0 || produc!=0){
+    let articulos=[];
         while(produc>0 || produc!=0){
-            let sum=0;
-            if(produc=='1'){
-                articulo='Cuadro';
-                sum=1200;
-                produc=prompt('Articulo agregado: '+articulo+'. Ingrese numero de otro producto que desee comprar, caso contrario presione 0');
+            if(produc==cuadro.id){
+                produc=prompt('Articulo agregado: '+cuadro.nombreP+'. Ingrese numero de otro producto que desee comprar, caso contrario presione 0');
+                precio+=cuadro.precio;
+                articulos.push(cuadro.nombreP)
                 i++;
-            }else if(produc=='2'){
-                articulo='Mate';
-                sum=1000;
-                produc=prompt('Articulo agregado: '+articulo+'. Ingrese numero de otro producto que desee comprar, caso contrario presione 0');
+            }else if(produc==mate.id){
+                produc=prompt('Articulo agregado: '+mate.nombreP+'. Ingrese numero de otro producto que desee comprar, caso contrario presione 0');
+                precio+=mate.precio;
+                articulos.push(mate.nombreP)
                 i++;
-            }else if(produc=='3'){
-                articulo='Promoción';
-                sum=800;
-                produc=prompt('Articulo agregado: '+articulo+'. Ingrese numero de otro producto que desee comprar, caso contrario presione 0');
+            }else if(produc==promocion.id){
+                produc=prompt('Articulo agregado: '+promocion.nombreP+'. Ingrese numero de otro producto que desee comprar, caso contrario presione 0');
+                precio+=promocion.precio;
+                articulos.push(promocion.nombreP)
                 i++;
             }else{
                 produc=prompt('El valor ingresado no se encuentra en stock, por favor vuelva a ingresar un valor que este disponible, caso contrario presione 0 para salir, muchas gracias');
             }
-            precio+=sum;
-            if(i==1){
-                articulos=articulo;
-            }else if(produc<4){
-                articulos=articulos+'-'+articulo;
-            }else{
-                articulos=articulos;
-            }   
         }
-        alert('Monto total a pagar: $'+precio+', cantidad de articulos: '+i+' y articulos comprados: '+articulos)
-        alert('Gracias por su compra!')
-    }else{
-        alert('Gracias por su visita!');
-    }
+        if(i>0){
+            alert('Monto total a pagar: $'+precio+', cantidad de articulos: '+i+' y articulos comprados: '+articulos)
+            alert('Gracias por su compra!')
+        }else{
+            alert('Gracias por su visita!');
+        }
+        
 }
 
 Bienvenido();
 let producto=parseInt(prompt('Ingrese numero de producto que desea comprar: 1-Cuadro(Precio: $1.200) 2-Mate(Precio:$1.000) 3-promoción(Precio:$800) - Caso contrario presione 0 para salir'));
 Compra(producto);
-
